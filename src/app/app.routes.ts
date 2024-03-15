f5568aceaa02fbde547a81bdd0ffbc552ab4f082
Abhishek0090 <palabhishek411@gmail.com>
2024-03-15 18:32:52 +0530

    first

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+export const routes: Routes = [
+    {
+        path : "", component : AppComponent
+    },
+    {
+    path : "about",component : AboutComponent 
+    },
+    {
+        path : "contact",component : ContactComponent 
+ },
+];
+
+ 
